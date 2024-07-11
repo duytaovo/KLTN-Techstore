c63912da4d2b426d01d43febcddd70e970d313e2
Võ Duy Tạo <duytaovo@gmail.com>
2024-07-12 06:14:11 +0700

    update

--- src/pages/ProductDetail/SmartPhoneDetail.tsx
+++ src/pages/ProductDetail/SmartPhoneDetail.tsx
@@ -169,8 +169,7 @@ export default function SmartPhoneDetail() {
       },
     );
     setProductSuggestList(response.data.data);
-    console.log(productSuggestList)
-    setIsLoading(false)
+    setIsLoading(false);
   };
 
   const updateBert = async () => {
